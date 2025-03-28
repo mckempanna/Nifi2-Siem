483dde624a2bb71934dbbd3debe25ad8dd44a097
Matt Dinep <matt.dinep@gmail.com>
2025-03-28 14:24:46 -0700

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,1 +1,1 @@
-/opt/splunk/bin/splunk search "index=main" -output csv -maxout 1000 -auth root:$Cleire7 > /home/mdinep/test.csv
+/opt/splunk/bin/splunk search "index=main" -output csv -maxout 1000 -auth root:$1 > /home/mdinep/test.csv
